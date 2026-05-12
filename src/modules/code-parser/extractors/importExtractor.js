47dff5945b3acaba1e39e219c2c6a85a2c2c11f2
Tadeo Sandoval <tadeolmd@gmail.com>
2026-05-11 19:36:16 -0700

    feat: implement class extraction logic in extractClasses function and remove redundant code from CodeParser

--- src/modules/code-parser/extractors/importExtractor.js
+++ src/modules/code-parser/extractors/importExtractor.js
@@ -1,5 +1,3 @@
-
-
 export function extractImports(ast) {
   const imports = [];
 
--- src/modules/code-parser/index.js
+++ src/modules/code-parser/index.js
@@ -105,41 +105,7 @@ class CodeParser {
    * - No debe detectar funciones
    * - No debe detectar exports
    */
-  #extractClasses(ast) {
-    const classes = [];
-
-    try {
-      const visit = (node) => {
-        if (!node || typeof node !== "object") {
-          return;
-        }
-        if (node.type === "ClassDeclaration") {
-          classes.push({
-            name: node.id?.name || "Anonymous",
-            extends: node.superClass?.name,
-            line: node.loc?.start.line || 0,
-            methods: this.#extractFunctions(node),
-          });
-        }
-        for (const key in node) {
-          if (key === "loc" || key === "range" || key === "comments") continue;
-          const child = node[key];
-          if (Array.isArray(child)) {
-            for (const item of child) {
-              visit(item);
-            }
-          } else if (typeof child === "object") {
-            visit(child);
-          }
-        }
-      };
-      visit(ast);
-      return classes;
-    } catch (error) {
-      console.error("Error extracting classes:", error);
-      return [];
-    }
-  }
+  
 
   /*
    * EXTRAE EXPORTS
