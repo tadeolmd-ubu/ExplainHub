1992d9acb0839071ba96358981f88808bda06c66
Tadeo Sandoval <tadeolmd@gmail.com>
2026-05-11 19:38:09 -0700

    feat: move extractCjsExport function to cjsExportExtractor module and remove from CodeParser

--- src/modules/code-parser/index.js
+++ src/modules/code-parser/index.js
@@ -208,45 +208,6 @@ class CodeParser {
       });
     }
   }
-  #extractCjsExport(node, exports) {
-    const left = node.left;
-    if (
-      left.type === "MemberExpression" &&
-      left.object.type === "Identifier" &&
-      left.object.name === "module" &&
-      left.property.type === "Identifier" &&
-      left.property.name === "exports"
-    ) {
-      exports.push({
-        name: "default",
-        kind: "cjs",
-        line: node.loc?.start.line || 0,
-      });
-      if (node.right?.type === "ObjectExpression") {
-        for (const prop of node.right.properties) {
-          if (prop.key?.name) {
-            exports.push({
-              name: prop.key.name,
-              kind: "cjs",
-              line: node.loc?.start.line || 0,
-            });
-          }
-        }
-      }
-    }
-    if (
-      left.type === "MemberExpression" &&
-      left.object.type === "Identifier" &&
-      left.object.name === "exports" &&
-      left.property.type === "Identifier"
-    ) {
-      exports.push({
-        name: left.property.name,
-        kind: "cjs",
-        line: node.loc?.start.line || 0,
-      });
-    }
-  }
 }
 
 export default CodeParser;
